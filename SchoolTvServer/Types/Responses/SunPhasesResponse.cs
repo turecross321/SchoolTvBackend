6b1afb44e3b92948428198f217e88884cce98895
Ture <51852312+turecross321@users.noreply.github.com>
2024-10-23 13:51:40 +0200

    Change to DateTimeOffsets for SunPhasesResponse.cs

--- SchoolTvServer/Types/Responses/SunPhasesResponse.cs
+++ SchoolTvServer/Types/Responses/SunPhasesResponse.cs
@@ -4,20 +4,20 @@ namespace SchoolTvServer.Types.Responses;
 
 public record SunPhasesResponse
 {
-    public DateTime? SolarNoon { get; set; }
-    public DateTime? Nadir { get; set; }
-    public DateTime? Sunrise { get; set; }
-    public DateTime? Sunset { get; set; }
-    public DateTime? SunriseEnd { get; set; }
-    public DateTime? SunsetStart { get; set; }
-    public DateTime? Dawn { get; set; }
-    public DateTime? Dusk { get; set; }
-    public DateTime? NauticalDawn { get; set; }
-    public DateTime? NauticalDusk { get; set; }
-    public DateTime? NightEnd { get; set; }
-    public DateTime? Night { get; set; }
-    public DateTime? GoldenHourEnd { get; set; }
-    public DateTime? GoldenHour { get; set; }
+    public DateTimeOffset? SolarNoon { get; set; }
+    public DateTimeOffset? Nadir { get; set; }
+    public DateTimeOffset? Sunrise { get; set; }
+    public DateTimeOffset? Sunset { get; set; }
+    public DateTimeOffset? SunriseEnd { get; set; }
+    public DateTimeOffset? SunsetStart { get; set; }
+    public DateTimeOffset? Dawn { get; set; }
+    public DateTimeOffset? Dusk { get; set; }
+    public DateTimeOffset? NauticalDawn { get; set; }
+    public DateTimeOffset? NauticalDusk { get; set; }
+    public DateTimeOffset? NightEnd { get; set; }
+    public DateTimeOffset? Night { get; set; }
+    public DateTimeOffset? GoldenHourEnd { get; set; }
+    public DateTimeOffset? GoldenHour { get; set; }
 
     public static SunPhasesResponse FromSunPhaseTimes(List<SunPhase> phases)
     {
@@ -40,9 +40,13 @@ public record SunPhasesResponse
         };
     }
 
-    private static DateTime? GetPhaseTime(List<SunPhase> phases, string phaseName)
+    private static DateTimeOffset? GetPhaseTime(List<SunPhase> phases, string phaseName)
     {
         SunPhase? phase = phases.FirstOrDefault(p => p.Name.Value == phaseName);
-        return phase?.PhaseTime;
+        DateTime? utc = phase?.PhaseTime;
+        if (utc != null)
+            return new DateTimeOffset((DateTime)utc);
+
+        return null;
     }
 }
